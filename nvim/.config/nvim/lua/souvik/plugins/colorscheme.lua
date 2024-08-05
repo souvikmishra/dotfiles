248c680af03bc7d611aaab742d377bbc08f19380
Souvik Mishra <mishra.souvik911@gmail.com>
2024-08-05 14:26:53 +0530

    update colorscheme flavour

--- nvim/.config/nvim/lua/souvik/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/souvik/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        transparent_background = true,
-      })
-      -- load the colorscheme here
-      vim.cmd([[colorscheme catppuccin]])
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("catppuccin").setup({
+				flavour = "macchiato",
+				transparent_background = true,
+			})
+			-- load the colorscheme here
+			vim.cmd([[colorscheme catppuccin]])
+		end,
+	},
 }
