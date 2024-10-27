0762dc99a2282c8350f4de36fd92ad3bba7cd654
Souvik Mishra <mishra.souvik911@gmail.com>
2024-10-27 14:57:10 +0530

    update general keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,23 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap -- for conciseness
+
+-- General Keymaps -------------------
+
+-- use jk to exit insert mode
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+-- use JK to move lines up and down in visual mode
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- delete single character without copying into register
+-- keymap.set("n", "x", '"_x')
+
+-- center screen when scrolling up and down
+keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up with cursor in the middle" })
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down with cursor in the middle" })
